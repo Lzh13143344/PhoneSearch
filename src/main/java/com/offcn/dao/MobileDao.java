87b35fa721b51a50401a68e88ffd3919864f4562
lzh3344 <lzh1413@126.com>
2018-08-15 21:29:02 +0800

    dao层接口修改 08-15

--- src/main/java/com/offcn/dao/MobileDao.java
+++ src/main/java/com/offcn/dao/MobileDao.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import com.offcn.po.Mobile;
 
-@Repository
+
 public interface MobileDao {
 
 	public Mobile getByMNumber(@Param("mobileNumber") String mobileNumber);
