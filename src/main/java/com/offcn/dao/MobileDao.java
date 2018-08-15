9c97ac21c79fc584fed23808f04bfe5299ed339c
Lzh13143344 <42401738+Lzh13143344@users.noreply.github.com>
2018-08-15 22:15:52 +0800

    Update MobileDao.java

--- src/main/java/com/offcn/dao/MobileDao.java
+++ src/main/java/com/offcn/dao/MobileDao.java
@@ -5,8 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import com.offcn.po.Mobile;
 
-
 public interface MobileDao {
-
+       //查询
 	public Mobile getByMNumber(@Param("mobileNumber") String mobileNumber);
 }
